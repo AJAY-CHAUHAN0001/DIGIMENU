9e01efc90e0b39a340fdea654b05859f07004df5
Ajay Chauhan <152907284+AJAY-CHAUHAN0001@users.noreply.github.com>
2024-10-12 16:05:34 +0530

    Update BookAtable.jsx

--- Frontend/src/components/BookAtable.jsx
+++ Frontend/src/components/BookAtable.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Spinner from "react-bootstrap/Spinner";
 
-const booktableApi = "http://localhost:5000/booktables";
+const booktableApi = "https://digimenu-backend.onrender.com/booktables";
 
 export const BookAtable = () => {
   const navigate = useNavigate();
