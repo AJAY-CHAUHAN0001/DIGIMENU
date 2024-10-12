ff748ccc22d0e8c1ca43a2b0e2b564d46d89c890
Ajay Chauhan <152907284+AJAY-CHAUHAN0001@users.noreply.github.com>
2024-10-12 21:32:30 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const { body, validationResult } = require('express-validator')
 app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = 5000;
+const PORT = Process.env || 5000;
 // const cors = require('cors');
 
 // const corsOptions = {
